32859bf2b196a2d2114f0242b4e08fe54ef92dc5
shajn786 <shajn786@gmail.com>
2022-12-12 12:03:45 +0530

    initial

---
+++ src/main/java/com/example/nest_digital_app_new/NestDigitalAppNewApplication.java
@@ -0,0 +1,13 @@
+package com.example.nest_digital_app_new;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NestDigitalAppNewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NestDigitalAppNewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/nest_digital_app_new/NestDigitalAppNewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nest_digital_app_new;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NestDigitalAppNewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
